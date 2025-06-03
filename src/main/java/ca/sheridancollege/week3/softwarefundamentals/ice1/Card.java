3f81b0f83290d8f8bbbe47289fee83c5c1115133
ChaulaDevi <ChaulaDevi@localhost>
2025-06-02 21:57:18 -0400

    Completed CardTrick class with random cards and user input

--- src/main/java/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/main/java/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
-/**
- *
- * @author ChaulaDevi
- */
+
 public class Card {
-    
+
+   private String suit;
+   private int value;
+
+   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+   public String getSuit() {
+       return suit;
+   }
+
+   public void setSuit(String suit) {
+       this.suit = suit;
+   }
+
+   public int getValue() {
+       return value;
+   }
+
+   public void setValue(int value) {
+       this.value = value;
+   }
 }
+
--- src/main/java/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/main/java/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
-/**
- *
- * @author ChaulaDevi
- */
+import java.util.Random;
+
+
 public class CardTrick {
-    
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+        Random rand = new Random();
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(rand.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i] = c;
+        }
+
+        // Hardcoded lucky card
+        Card luckyCard = new Card();
+        luckyCard.setValue(5);
+        luckyCard.setSuit("Hearts");
+
+        boolean found = false;
+        for (Card c : magicHand) {
+            if (c.getValue() == luckyCard.getValue() &&
+                c.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Lucky card FOUND in the magic hand! 🎉");
+        } else {
+            System.out.println("Lucky card NOT found. ❌");
+        }
+
+        // Show cards in the magic hand
+        System.out.println("\nMagic Hand:");
+        for (Card c : magicHand) {
+            System.out.println(c.getValue() + " of " + c.getSuit());
+        }
+    }
 }
